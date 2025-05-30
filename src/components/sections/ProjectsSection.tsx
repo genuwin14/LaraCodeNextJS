161ba01459202a4aa3fdd545057bef7a44e3d507
Kristian Bas <genuinegaming14@gmail.com>
2025-05-30 21:03:59 +0800

    Rmove import not used in ProjectsSection.tsx

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { SetStateAction, useState } from "react"
+import { useState } from "react"
 import Image from "next/image"
 import { X, ExternalLink, Github } from "lucide-react"
 
